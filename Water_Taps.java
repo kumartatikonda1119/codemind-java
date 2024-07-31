d332ffa5f0dbd099bbd788a896a2f868070dbde1
Kumar Tatikonda <147791707+kumartatikonda1119@users.noreply.github.com>
2024-07-31 15:03:17 +0530

    import java.util.*; public class Main {     public static void main(String[] args)     {         Scanner s=new Scanner (System.in);         int a,b;         a=s.nextInt();         b=s.nextInt();         int c=(a*b)/(a+b);         System.out.print(c);
        }
    }

---
+++ Water_Taps.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner s=new Scanner (System.in);
+        int a,b;
+        a=s.nextInt();
+        b=s.nextInt();
+        int c=(a*b)/(a+b);
+        System.out.print(c);
+        
+    }
+}
