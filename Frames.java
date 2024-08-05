989cc10a5d98105b0e16c10f8849640096dfb49c
Kumar Tatikonda <147791707+kumartatikonda1119@users.noreply.github.com>
2024-08-05 14:57:04 +0530

    import java.util.*; public class kiran{     public static void main(String[] args)     {         Scanner kumar=new Scanner (System.in);         int a,b,c;         a=kumar.nextInt();         b=kumar.nextInt();         c=kumar.nextInt();         System.out.println(2*(a+b)*c);
        }
    }

---
+++ Frames.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class kiran{
+    public static void main(String[] args)
+    {
+        Scanner kumar=new Scanner (System.in);
+        int a,b,c;
+        a=kumar.nextInt();
+        b=kumar.nextInt();
+        c=kumar.nextInt();
+        System.out.println(2*(a+b)*c);
+        
+        
+    }
+}
